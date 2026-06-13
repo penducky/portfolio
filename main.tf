0740c326ef765b31273010eba69b5c1b1b6d0821
penducky <deven1232@gmail.com>
2026-06-13 18:57:45 +0700

    Add the root domain to the certificate

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_s3_bucket_policy" "main" {
 ## ACM CERTIFICATE FOR CLOUDFRONT ##
 resource "aws_acm_certificate" "cert" {
   domain_name       = local.domain_name
-  subject_alternative_names = ["*.${local.domain_name}"]
+  subject_alternative_names = [local.domain_name, "*.${local.domain_name}"]
   validation_method = "DNS"
 
   tags = {
